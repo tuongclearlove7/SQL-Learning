34fc8c0d0c08ce56576306f8dd497ab4f07da9cc
Clearlove7 <88828156+tuongclearlove7@users.noreply.github.com>
2024-05-03 22:46:23 +0700

    update source code

---
+++ SQL server/HE_QUAN_TRI_CSDL/47_IS401T_TrầnThếTường_27211239157.sql
@@ -0,0 +1,617 @@
+﻿--															ĐỒ ÁN CÁ NHÂN
+-- Môn học: Hệ Quản Trị Cơ Sở Dữ Liệu
+-- Họ và tên: Trần Thế Tường
+-- Lớp: IS401T
+-- mssv: 27211239157
+
+--CREATE DATABASE
+create database QUANLYLUONG
+use QUANLYLUONG
+
+--CREATE TABLE
+
+CREATE TABLE nhanvien (
+    manv VARCHAR(20) PRIMARY KEY,
+    hoten NVARCHAR(255),
+    ngaysinh DATETIME,
+    phai int,
+    quequan VARCHAR(255),
+    sodt VARCHAR(20),
+    mapb VARCHAR(20),
+	macv VARCHAR(20),
+);
+
+CREATE TABLE phongban (
+    mapb VARCHAR(20) PRIMARY KEY,
+	tenpb NVARCHAR(255),
+
+);
+
+
+CREATE TABLE chucvu (
+    macv VARCHAR(20) PRIMARY KEY,
+	tencv NVARCHAR(255),
+	hscv decimal,
+
+);
+
+CREATE TABLE hesoluong (
+    thang int,
+	hsl decimal,
+	manv VARCHAR(20),
+
+);
+
+CREATE TABLE tamung (
+    matu INT PRIMARY KEY,
+    manv VARCHAR(20),
+    TONGTIEN DECIMAL(10, 2),
+);
+
+CREATE TABLE ct_tamung (
+    stt INT PRIMARY KEY,
+    ngaythang DATE,
+    manv VARCHAR(20),
+    matu INT,
+    sotien DECIMAL(10, 2),
+   
+);
+
+ALTER TABLE nhanvien
+ADD CONSTRAINT nhanvien_Fk_phongban
+    FOREIGN KEY (mapb) REFERENCES phongban(mapb);
+
+ALTER TABLE nhanvien
+ADD CONSTRAINT nhanvien_Fk_chucvu
+    FOREIGN KEY (macv) REFERENCES chucvu(macv);
+
+ALTER TABLE hesoluong
+ADD CONSTRAINT hesoluong_Fk_nhanvien
+    FOREIGN KEY (manv) REFERENCES nhanvien(manv);
+
+ALTER TABLE tamung
+ADD CONSTRAINT tamung_Fk_nhanvien
+    FOREIGN KEY (manv) REFERENCES nhanvien(manv);
+
+ALTER TABLE ct_tamung
+ADD CONSTRAINT ct_tamung_Fk_nhanvien
+    FOREIGN KEY (manv) REFERENCES nhanvien(manv);
+
+ALTER TABLE ct_tamung
+ADD CONSTRAINT ct_tamung_Fk_tamung
+    FOREIGN KEY (matu) REFERENCES tamung(matu);
+
+-- Insert dữ liệu vào các bảng đã tạo
+
+INSERT INTO phongban(mapb, tenpb)
+VALUES('pb01', N'Phòng kế toán'),
+('pb02', N'Phòng marketing'),
+('pb03', N'Phòng công nghệ thông tin'),
+('pb04', N'Phòng nhân sự'),
+('pb05', N'Phòng dịch vụ');
+
+INSERT INTO phongban(mapb, tenpb)
+VALUES('pb01', N'Phòng kế toán'),
+('pb02', N'Phòng marketing'),
+('pb03', N'Phòng công nghệ thông tin'),
+('pb04', N'Phòng nhân sự'),
+('pb05', N'Phòng dịch vụ');
+
+INSERT INTO nhanvien (manv, hoten, ngaysinh, phai, quequan, sodt, mapb) VALUES
+('nv01', N'Trần Thế Tường', '1990-01-01', 1, 'Da Nang', '123456789', 'pb01'),
+('nv02', N'Nguyễn Ngọc Khánh', '1992-05-15', 0, 'Da Nang', '987654321', 'pb02'),
+('nv03', N'Nguyễn Thị Cúc', '1988-11-20', 1, 'Dak Lak', '555555555', 'pb01'),
+('nv04', N'Trần Thị Mai', '1995-03-10', 1, 'Da Nang', '777777777', 'pb03'),
+('nv05', N'Trần Vinh Quang', '1998-07-02', 0, 'Dak Lak', '888888888', 'pb05'),
+('nv06', N'Hồ Phương Thảo', '1993-09-25', 1, 'Da Nang', '444444444', 'pb04'),
+('nv07', N'Hồ Nhật Anh', '1996-12-30', 0, 'Da Nang', '666666666', 'pb04'),
+('nv08', N'Trần Lê Đức Nhật Anh', '1991-04-05', 1, 'Da Nang', '999999999', 'pb02'),
+('nv09', N'Nguyễn Văn Phát', '1987-08-15', 1, 'TP HCM', '111111111', 'pb05'),
+('nv10', N'Nguyễn Thu Phương', '1994-06-18', 0, 'Binh Dinh', '222222222', 'pb04');
+
+INSERT INTO chucvu(macv, tencv, hscv, manv) VALUES
+('cv01', N'Chức vụ Trưởng phòng', 1.5, 'nv01'),
+('cv02', N'Chức vụ Trưởng phòng', 2.0, 'nv02'),
+('cv03', N'Chức vụ nhân viên IT', 1.8, 'nv03'),
+('cv04', N'Chức vụ nhân viên IT', 1.7, 'nv04'),
+('cv05', N'Chức vụ nhân viên Kế toán', 2.2, 'nv05');
+
+INSERT INTO hesoluong (thang, hsl, manv) VALUES
+(1, 1.2, 'nv01'),
+(1, 1.0, 'nv02'),
+(1, 1.5, 'nv03'),
+(2, 1.8, 'nv04'),
+(2, 1.3, 'nv05'),
+(2, 1.7, 'nv06'),
+(3, 2.0, 'nv07'),
+(3, 1.5, 'nv08'),
+(3, 1.9, 'nv09'),
+(4, 1.6, 'nv10'),
+(4, 1.4, 'nv01'),
+(4, 1.1, 'nv02'),
+(5, 1.3, 'nv03'),
+(5, 1.7, 'nv04'),
+(5, 1.5, 'nv05');
+
+INSERT INTO TAMUNG (MATU, MANV, TONGTIEN) VALUES
+(1, 'nv01', 500000.00),
+(2, 'nv02', 700000.00),
+(3, 'nv03', 600000.00),
+(4, 'nv04', 800000.00),
+(5, 'nv04', 900000.00),
+(6, 'nv05', 750000.00),
+(7, 'nv05', 600000.00),
+(8, 'nv02', 850000.00),
+(9, 'nv03', 700000.00),
+(10, 'nv01', 750000.00);
+
+-- SELECT 
+--cau 1: Tính lương cho nhân viên
+select nhanvien.manv, ngaysinh, tencv, 
+hsl, thang, hsl*1890000 * hscv as luong 
+from nhanvien, hesoluong, chucvu
+where nhanvien.macv = chucvu.macv and
+nhanvien.manv = hesoluong.manv 
+group by nhanvien.manv, nhanvien.ngaysinh, 
+chucvu.tencv, hesoluong.hsl, hesoluong.thang, chucvu.hscv
+
+--cau 2: đếm số nhân viên của mỗi phòng ban
+select phongban.mapb, tenpb, COUNT(*) as SONHANVIEN
+from phongban, nhanvien
+where  phongban.mapb = nhanvien.mapb
+group by phongban.mapb, phongban.tenpb
+
+-- cau 3: đếm số nhân viên của mỗi chức vụ
+select chucvu.macv, tencv, COUNT(*) as SONHANVIEN 
+from nhanvien, chucvu
+where nhanvien.macv = chucvu.macv
+group by chucvu.macv, tencv
+
+-- cau 4: Tính tổng lương của mỗi phòng ban
+select phongban.mapb, tenpb, sum(hsl * 1890000 * hscv) as TONGLUONG
+from nhanvien, phongban, hesoluong, chucvu
+where phongban.mapb = nhanvien.mapb and
+nhanvien.manv = hesoluong.manv
+group by phongban.mapb, tenpb
+
+-- cau 5 hiển thị 2 phòng ban có tổng lương nhiều nhất
+select top 2 phongban.mapb, tenpb, 
+sum(hsl * 1890000 * hscv) as TONGLUONG
+from nhanvien, phongban, hesoluong, chucvu
+where phongban.mapb = nhanvien.mapb and
+nhanvien.manv = hesoluong.manv
+group by phongban.mapb, tenpb 
+order by sum(hsl * 1890000 * hscv) desc
+
+-- cau 6: Hiển thị tổng lương theo tháng
+select hesoluong.thang ,sum(hsl * 1890000 * hscv) as TONGLUONG
+from nhanvien, hesoluong, chucvu
+where nhanvien.manv = hesoluong.manv
+group by hesoluong.thang
+
+-- cau 7: Hiển thị 3 nhân viên có tổng lương nhiều nhất
+select top 3 nhanvien.manv, ngaysinh, tencv, 
+hsl, thang, hsl*1890000 * hscv as luong 
+from nhanvien, hesoluong, chucvu
+where nhanvien.macv = chucvu.macv and
+nhanvien.manv = hesoluong.manv 
+group by nhanvien.manv, nhanvien.ngaysinh, 
+chucvu.tencv, hesoluong.hsl, hesoluong.thang, chucvu.hscv
+order by luong desc
+
+-- cau 8: Cập nhật HSL tăng 0.5 theo mã chức vụ
+UPDATE hesoluong
+SET hsl = hsl + 0.5
+FROM hesoluong, chucvu, nhanvien
+WHERE hesoluong.manv = nhanvien.manv and 
+chucvu.macv = nhanvien.macv and
+chucvu.tencv IN ('GD', 'PGD');
+
+-- cau 9: cập nhật hscv tăng 0.5 của những nhân viên có giới tính nữ
+UPDATE chucvu
+SET hscv = hscv + 0.5
+FROM nhanvien, chucvu
+WHERE nhanvien.macv = chucvu.macv and 
+nhanvien.phai IN (0);
+
+-- cau 10: tạo thủ tục hiển thị danh sách 
+-- phongban.mapb, tenpb, hoten, ngaysinh, phai, hsl, hscv, thang, hsl * 1890000 * hscv AS luong
+create PROCEDURE HIENTHIDANHSACH
+AS
+BEGIN
+    SELECT phongban.mapb, tenpb, hoten, ngaysinh, phai, hsl, hscv, thang, hsl * 1890000 * hscv AS luong
+	from phongban, nhanvien , hesoluong, chucvu
+	where phongban.mapb = nhanvien.mapb and
+	nhanvien.macv = chucvu.macv and
+	nhanvien.manv = hesoluong.manv
+	group by phongban.mapb, tenpb, hoten, ngaysinh, phai, hsl, hscv, thang
+END
+
+exec HIENTHIDANHSACH
+
+-- PROCEDURE
+-- cau 1: Tạo thủ tục thêm mới nhân viên
+create PROCEDURE THEMMOINHANVIEN
+AS
+BEGIN
+    INSERT INTO nhanvien(manv, hoten, ngaysinh, phai, quequan, sodt, mapb, macv)
+	VALUES('nv11', N'Nhân viên 11', '2002-01-06', 1, N'Nam Định', N'0915662495', 'pb05', 'cv05');
+END
+
+exec THEMMOINHANVIEN;
+
+-- cau 2: Tạo thủ tục thêm mới hệ số lương của nhân viên
+create PROCEDURE THEMMOINHANVIENVAOHESOLUONG
+AS
+BEGIN
+    INSERT INTO hesoluong(thang, hsl, manv)
+	VALUES(1, 1.5, 'nv11');
+END
+
+exec THEMMOINHANVIENVAOHESOLUONG;
+
+-- cau 3: tạo thủ tục Hiển thị danh sách nhân viên theo mapb và macv
+create PROCEDURE HIENTHIDANHSACHNHANVIENTHEOMACVVAMAPB
+@mapb varchar(20), @macv varchar(20)
+AS
+BEGIN
+    SELECT * FROM nhanvien where mapb = @mapb and macv = @macv
+END
+
+exec HIENTHIDANHSACHNHANVIENTHEOMACVVAMAPB @mapb='pb01', @macv='cv01'
+
+-- cau 4: Tạo thủ tục đếm số nhân viên có hệ số lương >= 3.0
+ALTER PROCEDURE THONGKESONHANVIENCOHSL
+@HSL FLOAT
+AS BEGIN
+DECLARE @SOLUONGNHANVIEN INT;
+	SELECT  @SOLUONGNHANVIEN = COUNT(distinct manv)  
+	FROM  HESOLUONG 
+	WHERE HESOLUONG.HSL>=@HSL
+PRINT 'SO LUONG NHAN VIEN >=  '+CAST(@HSL AS VARCHAR(10)) +' LA : ' + CAST(@SOLUONGNHANVIEN AS VARCHAR(10));
+END;
+
+exec THONGKESONHANVIENCOHSL 3.0
+
+-- CAU 5 TAO PROCEDURE CO THAM SO DAU RA LA SO NHAN VIEN CUA MOI PHONG BAN
+CREATE PROCEDURE THAMSODAURADEMSONHANVIENMOIPHONGBAN
+    @SONVCUAMOIPHONGBAN INT OUTPUT,
+	@MAPB VARCHAR(10)
+
+AS
+BEGIN
+    SELECT  @SONVCUAMOIPHONGBAN = COUNT(NHANVIEN.MANV)
+    FROM nhanvien
+	WHERE nhanvien.MAPB=@MAPB
+    GROUP BY nhanvien.MAPB;
+END;
+
+DECLARE @DepartmentCount INT;
+EXEC THAMSODAURADEMSONHANVIENMOIPHONGBAN @DepartmentCount OUTPUT, 'pb01';
+SELECT @DepartmentCount AS THAMSODAURADEMSONHANVIENMOIPHONGBAN;
+
+-- cau 6: Tạo thủ tục tính số lương theo tháng
+ALTER PROCEDURE THAMSODAURATONGLUONGCUAMOITHANG
+    @TONGLUONG FLOAT OUTPUT,
+	@thang int
+AS
+BEGIN
+    SELECT  @TONGLUONG = SUM(hsl * 1890000 * hscv)
+    FROM nhanvien,hesoluong, chucvu
+	WHERE nhanvien.manv = hesoluong.manv and
+	chucvu.macv = nhanvien.macv and
+	thang = @thang
+END;
+
+DECLARE @MONTH INT;
+EXEC THAMSODAURATONGLUONGCUAMOITHANG @MONTH OUTPUT, 1
+SELECT @MONTH AS SOLUONGTHEOTHANG;
+
+-- CAU 7 TAO PROCEDURE CO THAM SO DAU RA THONG KE SO NHAN VIEN CO HSCV >=X
+CREATE PROCEDURE SONHANVIENTHEOHSCV
+@HSCV FLOAT,
+@SOLUONGNHANVIENVIEN INT OUTPUT
+AS BEGIN 
+SELECT @SOLUONGNHANVIENVIEN =COUNT(NHANVIEN.MANV)
+FROM NHANVIEN, CHUCVU WHERE NHANVIEN.MACV=CHUCVU.MACV AND CHUCVU.HSCV>=@HSCV
+END;
+
+DECLARE @TONGNHANVIEN INT;
+EXEC SONHANVIENTHEOHSCV 1.5, @TONGNHANVIEN OUTPUT;
+SELECT @TONGNHANVIEN AS TONGSONHANVIENTHEOHESOCHUCVU;
+
+-- Trigger
+
+-- cau 1 Tạo bảng tạm ứng
+CREATE TABLE tamung (
+    matu INT PRIMARY KEY,
+    manv VARCHAR(20),
+    TONGTIEN DECIMAL(10, 2),
+);
+-- NHAP DL 10 DONG
+INSERT INTO TAMUNG (MATU, MANV, TONGTIEN) VALUES
+(1, 'nv01', 500000.00),
+(2, 'nv02', 700000.00),
+(3, 'nv03', 600000.00),
+(4, 'nv04', 800000.00),
+(5, 'nv05', 900000.00),
+(6, 'nv06', 750000.00),
+(7, 'nv07', 600000.00),
+(8, 'nv08', 850000.00),
+(9, 'nv09', 700000.00),
+(10, 'nv10', 750000.00);
+
+-- cau 2 Tạo bảng chi tiết tạm ứng
+CREATE TABLE ct_tamung (
+    stt INT PRIMARY KEY,
+    ngaythang DATE,
+    manv VARCHAR(20),
+    matu INT,
+    sotien DECIMAL(10, 2),
+   
+);
+--cau 3: Tạo trigger tạm ứng số tiền
+CREATE TRIGGER TAMUNG_SOTIEN1
+ON ct_tamung
+AFTER INSERT
+AS
+BEGIN
+    SET NOCOUNT ON;
+    UPDATE tamung
+    SET tongtien = tongtien - tam.sotien
+    FROM tamung, inserted tam
+    WHERE tamung.manv = tam.manv
+END;
+
+--cau 4: Thực thi trigger  tạm ứng số tiền
+INSERT INTO ct_tamung(stt, ngaythang, manv, matu, sotien)
+VALUES (1, '2024-02-25', 'nv01', 1, 100000.00);
+
+-- cau 5: Tạo trigger hủy tạm ứng
+CREATE TRIGGER NHANVIEN_HUYKHONGTAMUNGNUA1
+ON ct_tamung
+AFTER DELETE
+AS
+BEGIN
+    SET NOCOUNT ON;
+    UPDATE tamung
+    SET tongtien = tongtien + huytu.sotien
+    FROM tamung, deleted huytu
+    WHERE tamung.manv = huytu.manv AND huytu.manv= 'nv01';
+END;
+
+delete from ct_tamung where manv = 'nv01';
+
+-- cau 6: Thực thi trigger hủy tạm ứng
+alter TRIGGER UNGTHEM_SOVOIBANDAU1
+ON ct_tamung
+AFTER UPDATE
+AS
+BEGIN
+    SET NOCOUNT ON;
+    IF UPDATE(sotien) 
+    BEGIN
+		DECLARE @tien decimal(10,2)
+        UPDATE ct_tamung
+        SET sotien = sotien + 100000.00
+        WHERE manv = 'nv01'
+        UPDATE tamung
+        SET tongtien= tongtien - 100000.00
+        WHERE manv IN ('nv01');
+    END
+END;
+
+update ct_tamung  set sotien = sotien where manv = 'nv01';
+
+-- CAU 7 TAO TRIGGER KIEM SOAT DIEM KIEM KHI CO NHAN VIEN NAO DO TAM UNG TRONG 
+-- TRUONG HOP NEU NHAN VIEN TAM UNG VOI SO TIEN > TIEN 
+-- TRONG TAM UNG THI IN RA TAM UNG VUOT SO CHO PHEP.
+CREATE TRIGGER KIEMSOATUNGTIEN1
+ON ct_tamung
+AFTER UPDATE
+AS
+BEGIN
+    SET NOCOUNT ON;
+    IF UPDATE(sotien) 
+    BEGIN
+        DECLARE @tien DECIMAL(10,2);
+        DECLARE @maxTongTien DECIMAL(10,2);
+        SELECT TOP 1 @tien = tam.sotien
+        FROM inserted tam
+        WHERE tam.manv = 'nv01'
+        ORDER BY tam.sotien;
+        SELECT TOP 1 @maxTongTien = tongtien
+        FROM tamung
+        WHERE manv = 'nv01'
+        ORDER BY tongtien DESC;
+        BEGIN TRY
+            IF @tien IS NOT NULL AND @tien > @maxTongTien
+            BEGIN
+                PRINT N'Không đủ tiền!';
+            END
+            ELSE
+            BEGIN
+                UPDATE ct_tamung
+                SET sotien = sotien + @tien
+                WHERE manv = 'nv01';
+
+                UPDATE tamung
+                SET tongtien = tongtien - @tien
+                WHERE manv = 'nv01';
+            END
+        END TRY
+        BEGIN CATCH
+            PRINT N'Xử lý lỗi ở đây nếu có';
+        END CATCH
+    END
+END
+
+UPDATE ct_tamung SET sotien = sotien + 20000000.00 WHERE manv = 'nv01';
+
+-- FUNCTION
+-- CAU 1 TAO FUNCTION THONG KE VA IN RA SO NHAN VIEN NAM, NU CUA MOI PHONG BAN
+ALTER FUNCTION THONGKE_SONHANVIENNAMNU (@gioitinh int, @mapb varchar(50))
+RETURNS TABLE
+AS
+RETURN (
+    SELECT mapb, COUNT(*) as sonhanvien
+    FROM nhanvien
+    WHERE phai = @gioitinh and mapb = @mapb group by mapb
+);
+SELECT * FROM dbo.THONGKE_SONHANVIENNAMNU(1, 'pb01'); 
+SELECT * FROM dbo.THONGKE_SONHANVIENNAMNU(0, 'pb02'); 
+
+-- CAU 2 TAO FUNCTION THONG KE VA IN RA SO NHAN VIEN THEO TUNG CHUC VU
+CREATE FUNCTION THONGKE_SONHANVIENTHEOCHUCVU (@macv varchar(50))
+RETURNS TABLE
+AS
+RETURN (
+    SELECT macv, COUNT(*) as sonhanvien
+    FROM nhanvien
+    WHERE macv = @macv group by macv
+);
+SELECT * FROM dbo.THONGKE_SONHANVIENTHEOCHUCVU('cv01'); 
+SELECT * FROM dbo.THONGKE_SONHANVIENTHEOCHUCVU('cv02'); 
+SELECT * FROM dbo.THONGKE_SONHANVIENTHEOCHUCVU('cv03'); 
+
+-- CAU 3 TAO FUNCTION HIEN THI DS CAC NHAN VIEN THEO TUNG PHONG BAN
+CREATE FUNCTION HIEN_DS_NHANVIEN_THEOTUNGPHONGBAN (@mapb varchar(50))
+RETURNS TABLE
+AS
+RETURN (
+    SELECT  *
+    FROM nhanvien
+    WHERE mapb = @mapb
+);
+SELECT * FROM dbo.HIEN_DS_NHANVIEN_THEOTUNGPHONGBAN('pb01'); 
+SELECT * FROM dbo.HIEN_DS_NHANVIEN_THEOTUNGPHONGBAN('pb02'); 
+
+
+-- CAU 4 TAO FUNCTION HIEN THI DS CAC NHAN VIEN DUOC TRA LUONG THEO TUNG THANG, DS NHAN VIEN GOM THONG TIN : MANV, HOTEN, NGAYSINH, PHAI, MAPB, TENCV, LUONG
+-- TRONG DO LUONG = 1845000 * HSL * HSCV
+CREATE FUNCTION HIENTHI_DANHSACH_LUONGNHANVIENTHEOTHANG (@thang int)
+RETURNS TABLE
+AS
+RETURN (
+    SELECT nhanvien.manv, hoten, ngaysinh, phai, mapb, tencv, 184500 * hsl * hscv as luong
+    FROM nhanvien, chucvu, hesoluong
+    WHERE nhanvien.manv = hesoluong.manv and 
+	nhanvien.macv = chucvu.macv and
+	thang = @thang
+);
+SELECT * FROM dbo.HIENTHI_DANHSACH_LUONGNHANVIENTHEOTHANG(1); 
+SELECT * FROM dbo.HIENTHI_DANHSACH_LUONGNHANVIENTHEOTHANG(2); 
+SELECT * FROM dbo.HIENTHI_DANHSACH_LUONGNHANVIENTHEOTHANG(3); 
+
+
+-- CAU 5 TAO FUNCTION TINH TONG LUONG CHO MOI PHONG BAN 
+CREATE FUNCTION TINHTONGLUONG_CHOMOIPB (@mapb varchar(50))
+RETURNS TABLE
+AS
+RETURN (
+     SELECT nhanvien.mapb, sum(184500 * hsl * hscv) as tongluong
+    FROM nhanvien, chucvu, hesoluong
+    WHERE nhanvien.manv = hesoluong.manv and 
+	nhanvien.macv = chucvu.macv and
+	nhanvien.mapb = @mapb group by nhanvien.mapb
+);
+SELECT * FROM dbo.TINHTONGLUONG_CHOMOIPB('pb01'); 
+SELECT * FROM dbo.TINHTONGLUONG_CHOMOIPB('pb02');
+SELECT * FROM dbo.TINHTONGLUONG_CHOMOIPB('pb03'); 
+SELECT * FROM dbo.TINHTONGLUONG_CHOMOIPB('pb04'); 
+SELECT * FROM dbo.TINHTONGLUONG_CHOMOIPB('pb05'); 
+
+-- CAU 6 TAO FUNCTION HIEN THI DANH SACH CAC NHAN VIEN CO NGAY SINH NHAT TRONG THANG
+CREATE FUNCTION HIENTHI_DANHSACH_NHANVIEN_CONGAYSNTRONGTHANG (@thang int)
+RETURNS TABLE
+AS
+RETURN (
+    SELECT  *
+    FROM nhanvien
+    WHERE month(ngaysinh) = @thang
+);
+SELECT * FROM dbo.HIENTHI_DANHSACH_NHANVIEN_CONGAYSNTRONGTHANG(1); 
+SELECT * FROM dbo.HIENTHI_DANHSACH_NHANVIEN_CONGAYSNTRONGTHANG(11); 
+
+-- TRANSACTION
+-- Tạo TRANSACTION đặt vé xem phim
+-- tạo database, table
+CREATE DATABASE QUANLYDATXEMPHIM;
+
+use QUANLYDATXEMPHIM
+
+CREATE TABLE Phim (
+    MaPhim INT PRIMARY KEY,
+    TenPhim NVARCHAR(100),
+    TheLoai NVARCHAR(50),
+    ThoiLuong INT
+);
+
+
+CREATE TABLE RapChieu (
+    MaRap INT PRIMARY KEY,
+    TenRap NVARCHAR(100),
+    DiaChi NVARCHAR(200)
+);
+
+
+CREATE TABLE SuatChieu (
+    MaSuatChieu INT PRIMARY KEY,
+    MaPhim INT,
+    MaRap INT,
+    ThoiGian DATETIME,
+	SoLuongVeConLai INT,
+    FOREIGN KEY (MaPhim) REFERENCES Phim(MaPhim),
+    FOREIGN KEY (MaRap) REFERENCES RapChieu(MaRap)
+);
+
+
+CREATE TABLE NguoiDung (
+    MaNguoiDung INT PRIMARY KEY,
+    HoTen NVARCHAR(100),
+    Email NVARCHAR(100),
+    MatKhau NVARCHAR(50)
+);
+
+
+CREATE TABLE Ve (
+    MaVe INT PRIMARY KEY,
+    MaSuatChieu INT,
+    MaNguoiDung INT,
+    SoLuong INT,
+    GiaTien DECIMAL(10, 2),
+    FOREIGN KEY (MaSuatChieu) REFERENCES SuatChieu(MaSuatChieu),
+    FOREIGN KEY (MaNguoiDung) REFERENCES NguoiDung(MaNguoiDung)
+);
+
+
+BEGIN TRANSACTION;
+DECLARE @MaSuatChieu INT, @MaNguoiDung INT, @SoLuongVe INT, @Mave INT;
+SET @MaSuatChieu = 1; 
+SET @MaNguoiDung = 1; 
+SET @SoLuongVe = 2; 
+SET @Mave = 2;
+
+INSERT INTO Ve (MaVe,MaSuatChieu, MaNguoiDung, SoLuong, GiaTien)
+VALUES (@Mave,@MaSuatChieu, @MaNguoiDung, @SoLuongVe, @SoLuongVe * 10000);
+
+UPDATE SuatChieu
+SET SoLuongVeConLai = SoLuongVeConLai - @SoLuongVe
+WHERE MaSuatChieu = @MaSuatChieu;
+
+IF @@ERROR <> 0
+BEGIN
+    ROLLBACK;
+    PRINT N'Đặt vé thất bại. Hủy bỏ transaction.';
+END
+ELSE
+BEGIN
+    COMMIT;
+    PRINT N'Đặt vé thành công. Commit transaction.';
+END
+
+select * from Ve
+select * from SuatChieu
+select * from NguoiDung
+select * from RapChieu
+select * from Phim
---
+++ SQL server/HE_QUAN_TRI_CSDL/IS401T_10032024_TrầnThếTường_39157_QLTHUESACH.sql
@@ -0,0 +1,360 @@
+﻿CREATE DATABASE QUANLYCHOTHUESACH
+
+DROP DATABASE QUANLYCHOTHUESACH
+
+CREATE TABLE TACGIA (
+    matg VARCHAR(20) PRIMARY KEY,
+    tentg NVARCHAR(50)
+);
+
+CREATE TABLE SACH (
+    masach VARCHAR(20) PRIMARY KEY,
+    tensach NVARCHAR(50),
+    namxb INT,
+    slsach INT,
+    matg VARCHAR(20)
+);
+
+CREATE TABLE DOCGIA (
+    madg VARCHAR(20) PRIMARY KEY,
+    tendg NVARCHAR(50),
+    sodt VARCHAR(10)
+);
+
+CREATE TABLE PHIEUMUONTRA (
+    sophieu VARCHAR(20) PRIMARY KEY,
+    ngaymuon DATETIME,
+    ngaytra DATETIME,
+    tinhtrang NVARCHAR(50),
+    madg VARCHAR(20)
+);
+
+CREATE TABLE CHITIET (
+    madg VARCHAR(20),
+    masach VARCHAR(20),
+    slmuon INT,
+    dongia DECIMAL(10,2)
+);
+
+ALTER TABLE SACH
+ADD CONSTRAINT SACH_TACGIA
+FOREIGN KEY (MATG) REFERENCES TACGIA(MATG)
+
+ALTER TABLE PHIEUMUONTRA
+ADD CONSTRAINT PHIEUMUONTRA_DOCGIA
+FOREIGN KEY (MADG) REFERENCES DOCGIA(MADG)
+
+ALTER TABLE CHITIET
+ADD CONSTRAINT CHITIET_DOCGIA
+FOREIGN KEY (MADG) REFERENCES DOCGIA(MADG)
+
+ALTER TABLE CHITIET
+ADD CONSTRAINT CHITIET_SACH
+FOREIGN KEY (MASACH) REFERENCES SACH(MASACH)
+
+INSERT INTO TACGIA (MATG, TENTG) VALUES
+('TG01', N'Nguyễn Văn An'),
+('TG02', N'Trần Thị Bình'),
+('TG03', N'Lê Văn Cơ'),
+('TG04', N'Phạm Thị Duyên'),
+('TG05', N'Hoàng Văn Em');
+
+INSERT INTO SACH (MASACH, TENSACH, NAMXB, SLSACH, MATG) VALUES
+('S01', N'Sách Văn Hóa', 2020, 100, 'TG01'),
+('S02', N'Sách Giáo Dục', 2019, 150, 'TG02'),
+('S03', N'Sách Chính Trị', 2021, 120, 'TG03'),
+('S04', N'Sách Âm Nhạc', 2018, 200, 'TG04'),
+('S05', N'Sách Tiểu Thuyết', 2022, 90, 'TG05'),
+('S06', N'Sách Giải Trí', 2017, 180, 'TG01'),
+('S07', N'Sách Công Nghiệp', 2023, 110, 'TG02'),
+('S08', N'Sách Nông Nghiệp', 2016, 220, 'TG03'),
+('S09', N'Sách Quốc Phòng', 2024, 80, 'TG04'),
+('S10', N'Sách Công Nghệ', 2015, 250, 'TG05');
+
+INSERT INTO DOCGIA (MADG, TENDG, SODT) VALUES
+('DG01', N'Nguyễn Thị Xuyên', '0903456789'),
+('DG02', N'Trần Văn Yên', '0937654321'),
+('DG03', N'Lê Thị Tuyên', '0365478921'),
+('DG04', N'Phạm Văn Khánh', '0546782931'),
+('DG05', N'Hoàng Thị Linh', '0325469871'),
+('DG06', N'Nguyễn Văn Mãi', '0978654321'),
+('DG07', N'Trần Thị Nga', '0933456789'),
+('DG08', N'Lê Văn Ong', '0987654321'),
+('DG09', N'Phạm Hoàng Phước', '0365478921'),
+('DG10', N'Hoàng Văn Quyết', '0546782931');
+
+INSERT INTO PHIEUMUONTRA (SOPHIEU, NGAYMUON, NGAYTRA, TINHTRANG, MADG) VALUES
+('PM01', '2024-03-01', '2024-03-15', N'Đã trả', 'DG01'),
+('PM02', '2024-03-02', '2024-03-16', N'Đang Mượn', 'DG02'),
+('PM03', '2024-03-03', '2024-03-17', N'Đã trả', 'DG03'),
+('PM04', '2024-03-04', '2024-03-18', N'Đang Mượn', 'DG04'),
+('PM05', '2024-03-05', '2024-03-19', N'Đã trả', 'DG05'),
+('PM06', '2024-03-06', '2024-03-20', N'Đang Mượn', 'DG06'),
+('PM07', '2024-03-07', '2024-03-21', N'Đã trả', 'DG07'),
+('PM08', '2024-03-08', '2024-03-22', N'Đang Mượn', 'DG08'),
+('PM09', '2024-03-09', '2024-03-23', N'Đã trả', 'DG09'),
+('PM10', '2024-03-10', '2024-03-24', N'Đang Mượn', 'DG10'),
+('PM11', '2024-03-11', '2024-03-25', N'Đã trả', 'DG01'),
+('PM12', '2024-03-12', '2024-03-26', N'Đang Mượn', 'DG02'),
+('PM13', '2024-03-13', '2024-03-27', N'Đã trả', 'DG03'),
+('PM14', '2024-03-14', '2024-03-28', N'Đang Mượn', 'DG04'),
+('PM15', '2024-03-15', '2024-03-29', N'Đã trả', 'DG05'),
+('PM16', '2024-03-16', '2024-03-30', N'Đang Mượn', 'DG06'),
+('PM17', '2024-03-17', '2024-03-31', N'Đã trả', 'DG07'),
+('PM18', '2024-03-18', '2024-04-01', N'Đang Mượn', 'DG08'),
+('PM19', '2024-03-19', '2024-04-02', N'Đã trả', 'DG09'),
+('PM20', '2024-03-20', '2024-04-03', N'Đang Mượn', 'DG10');
+
+INSERT INTO CHITIET (MADG, MASACH, SLMUON, DONGIA) VALUES
+('DG01', 'S01', 2, 25000.00),
+('DG02', 'S02', 1, 30000.00),
+('DG03', 'S03', 3, 20000.00),
+('DG04', 'S04', 2, 22000.00),
+('DG05', 'S05', 1, 27000.00),
+('DG06', 'S06', 2, 18000.00),
+('DG07', 'S07', 3, 16000.00),
+('DG08', 'S08', 2, 21000.00),
+('DG09', 'S09', 1, 26000.00),
+('DG10', 'S10', 3, 15000.00),
+('DG01', 'S02', 1, 30000.00),
+('DG02', 'S03', 2, 20000.00),
+('DG03', 'S04', 3, 22000.00),
+('DG04', 'S05', 1, 27000.00),
+('DG05', 'S06', 2, 18000.00),
+('DG06', 'S07', 3, 16000.00),
+('DG07', 'S08', 2, 21000.00),
+('DG08', 'S09', 1, 26000.00),
+('DG09', 'S10', 3, 15000.00),
+('DG10', 'S01', 2, 25000.00),
+('DG01', 'S03', 1, 20000.00),
+('DG02', 'S04', 2, 22000.00),
+('DG03', 'S05', 3, 27000.00),
+('DG04', 'S06', 1, 18000.00),
+('DG05', 'S06', 3, 18000.00);
+
+-- Lập danh sách các độc giả có sođt bắt đầu là 093 và 090
+select * from DOCGIA 
+where sodt like '093%' or 
+sodt like '090%'
+
+-- Lập danh sách độc giả có ngày mượn từ 01/03/2024 đến 10/03/2024
+select sophieu, ngaymuon 
+from PHIEUMUONTRA 
+where YEAR(ngaymuon) = 2024 and 
+MONTH(ngaymuon) = 3 and 
+DAY(ngaymuon) <= 10
+
+-- Lập danh sách các độc giả chưa trả sách
+select * from PHIEUMUONTRA, DOCGIA where 
+DOCGIA.madg = PHIEUMUONTRA.madg and
+tinhtrang like N'%Đang%'
+
+-- thực hiện cập nhật đơn giá của sách S01 tăng 10%
+UPDATE CHITIET 
+SET dongia = dongia + dongia * 0.1 
+WHERE masach = 'S01';
+
+-- Thực tính thành tiền thuê sách cho các độc giả DOCGIA.madg, tendg, ngaymuon, tensach, namxb, slmuon
+SELECT  DOCGIA.madg, tendg, ngaymuon, tensach, namxb, 
+slmuon, dongia, sum(dongia * slmuon) as thanhtien
+FROM SACH, DOCGIA, PHIEUMUONTRA, CHITIET 
+WHERE SACH.masach = CHITIET.masach AND
+DOCGIA.madg = CHITIET.madg AND
+DOCGIA.madg = PHIEUMUONTRA.madg 
+GROUP by DOCGIA.madg, tendg, ngaymuon, tensach, namxb, 
+slmuon, dongia
+
+-- Thống kê số lượng sách của mỗi độc giả DOCGIA.madg, tendg, sosach
+SELECT DOCGIA.madg, tendg, count(masach) as sosach 
+FROM CHITIET,  DOCGIA 
+WHERE DOCGIA.madg = CHITIET.madg
+GROUP BY DOCGIA.madg, tendg
+
+-- Tạo PROCEDURE thêm mới dữ liệu vào bảng độc giả
+CREATE PROCEDURE THEMMOIDULIEU_DOCGIA1
+@madg varchar(20), @tendg nvarchar(255), @sodt varchar(50)
+AS
+BEGIN
+    INSERT INTO DOCGIA(madg, tendg, sodt)
+	VALUES(@madg, @tendg, @sodt);
+END
+
+exec THEMMOIDULIEU_DOCGIA1 @madg = 'DG11', @tendg = N'Trần Thế Tường', @sodt = '0915662495';
+
+-- tạo PROCEDURE tính tổng tiền thuê sách của mỗi độc giả
+CREATE PROCEDURE HIENTHITONGTIEN1
+AS
+BEGIN
+    SELECT  DOCGIA.madg, tendg, sum(dongia * slmuon) as tongtien
+	FROM DOCGIA, CHITIET 
+	WHERE  DOCGIA.madg = CHITIET.madg
+	GROUP by DOCGIA.madg, tendg, slmuon, dongia
+END
+
+EXEC HIENTHITONGTIEN1
+
+-- tạo PROCEDURE cho biết số độc giả mượn sách mỗi tháng
+CREATE PROCEDURE SODOCGIAMUONSACHMOITHANG1
+AS
+BEGIN
+    SELECT  MONTH(ngaymuon) as thang, count(*) as sodocgiamuon
+	FROM PHIEUMUONTRA 
+	GROUP BY MONTH(ngaymuon)
+END
+
+EXEC SODOCGIAMUONSACHMOITHANG1
+
+
+-- tạo PROCEDURE cho biết số độc giả trả sách mỗi tháng
+CREATE PROCEDURE SODOCGIATRASACHMOITHANG1
+AS
+BEGIN
+    SELECT  MONTH(ngaytra) as thang, count(*) as sodocgiatra
+	FROM PHIEUMUONTRA 
+	GROUP BY MONTH(ngaytra)
+END
+
+EXEC SODOCGIATRASACHMOITHANG1
+
+
+-- tao function in ra tong tien thue sach cho moi doc gia
+CREATE FUNCTION dbo.TONGTIENTHUESACHCUACACDOCGIA1 ()
+RETURNS TABLE
+AS
+RETURN (
+    SELECT madg, SUM(slmuon*dongia) as TONGTIEN 
+	FROM CHITIET
+    GROUP BY madg
+);
+
+
+
+SELECT * FROM dbo.TONGTIENTHUESACHCUACACDOCGIA1();
+
+
+-- tao function in ra so sach muon da tra tren moi phieu muon
+CREATE FUNCTION dbo.SOSACHDATRACUACACDOCGIA ()
+RETURNS TABLE
+AS
+RETURN (
+    SELECT PHIEUMUONTRA.madg, sophieu, COUNT(*) as SOSACHDATRA 
+	FROM CHITIET, PHIEUMUONTRA
+	WHERE CHITIET.madg = PHIEUMUONTRA.madg
+	AND PHIEUMUONTRA.tinhtrang = N'Đã trả'
+    GROUP BY PHIEUMUONTRA.madg, sophieu
+);
+
+
+
+SELECT * FROM dbo.SOSACHDATRACUACACDOCGIA();
+
+-- tao fucntion thong ke so sach cua moi nha xuat ban
+CREATE FUNCTION dbo.SOSACHCUACUAMOINHAXUATBAN1 ()
+RETURNS TABLE
+AS
+RETURN (
+    SELECT TACGIA.matg, COUNT(*) as SOSACH 
+	FROM SACH, TACGIA
+	WHERE TACGIA.matg = SACH.matg
+    GROUP BY TACGIA.matg
+);
+
+
+
+SELECT * FROM dbo.SOSACHCUACUAMOINHAXUATBAN1();
+
+-- Tạo PROCEDURE với tham số đầu ra là tổng tiền của mỗi phiếu mượn
+CREATE PROCEDURE TONGTIENCUAMOIPHIEUMUON1
+
+AS
+BEGIN
+    SELECT PHIEUMUONTRA.sophieu, SUM(slmuon * dongia) as TONGTIEN
+	FROM CHITIET, PHIEUMUONTRA 
+	WHERE CHITIET.madg = PHIEUMUONTRA.madg
+	GROUP BY PHIEUMUONTRA.sophieu
+END
+
+exec TONGTIENCUAMOIPHIEUMUON1;
+
+-- Tạo PROCEDURE với tham số đầu ra là so luong sach cua moi tac gia
+CREATE PROCEDURE SOLUONGSACHCUAMOITACGIA1
+
+AS
+BEGIN
+    SELECT TACGIA.matg, COUNT(*) as SOLUONGSACH
+	FROM TACGIA, SACH 
+	WHERE TACGIA.matg = SACH.matg
+	GROUP BY TACGIA.matg
+END
+
+exec SOLUONGSACHCUAMOITACGIA1;
+
+-- Tạo PROCEDURE in ra so luong doc gia muon, tra sach theo moi thang
+CREATE PROCEDURE SOLUONGSACHCUAMOITACGIA_PROCEDURE
+@thang int, @tinhtrangmuon NVARCHAR(255), @tinhtrangtra NVARCHAR(255)
+AS
+BEGIN
+	DECLARE @soluongdocgiamuon INT
+    DECLARE @soluongdocgiatra INT
+
+    SELECT @soluongdocgiamuon = COUNT(*)
+	FROM PHIEUMUONTRA
+	WHERE MONTH(ngaymuon) = @thang
+	AND tinhtrang = @tinhtrangmuon
+
+	SELECT @soluongdocgiatra =  COUNT(*)
+	FROM PHIEUMUONTRA
+	WHERE MONTH(ngaymuon) = @thang
+	AND tinhtrang =  @tinhtrangtra
+
+	PRINT N'Số lượng độc giả mượn sách là: ' + CAST(@soluongdocgiamuon AS NVARCHAR(10))
+    PRINT N'Số lượng độc giả trả sách là: ' + CAST(@soluongdocgiatra AS NVARCHAR(10))
+
+END
+
+
+exec SOLUONGSACHCUAMOITACGIA_PROCEDURE @thang = 3, 
+@tinhtrangmuon = N'Đang Mượn', 
+@tinhtrangtra = N'Đã trả';
+
+
+-- Tạo PROCEDURE cập nhật dongia tăng thêm 5000 cho tat ca sach thue
+CREATE PROCEDURE CAPNHATTHEM5000CHOTATCACACSACH1
+@sotien decimal
+AS
+BEGIN
+    UPDATE CHITIET SET  dongia = dongia + @sotien
+END
+
+exec CAPNHATTHEM5000CHOTATCACACSACH1 @sotien = 5000.0;
+
+
+-- Tạo PROCEDURE với tham số đầu ra là tổng tiền thuê sách của mỗi độc giả
+CREATE PROCEDURE TONGTIENTHUESACHCUAMOIDOCGIA2
+
+AS
+BEGIN
+    SELECT CHITIET.madg, SUM(slmuon * dongia) as TONGTIEN
+	FROM CHITIET
+	GROUP BY CHITIET.madg
+END
+
+exec TONGTIENTHUESACHCUAMOIDOCGIA2;
+
+
+select * from TACGIA
+select * from SACH
+select * from DOCGIA
+select * from PHIEUMUONTRA
+select * from CHITIET
+
+
+
+
+
+
+
+
+
+
+
